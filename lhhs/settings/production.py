5a315e2fb8a4bdfbb6fae5acd05dbf1cf9ae4997
Brad Rice <bradrice1@gmail.com>
2021-01-08 11:42:08 -0500

    Added hosts to production

--- lhhs/settings/production.py
+++ lhhs/settings/production.py
@@ -1,4 +1,6 @@
 from .base import *
+import json
+
 
 DEBUG = False
 
@@ -6,3 +8,22 @@ try:
     from .local import *
 except ImportError:
     pass
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+ANYMAIL = {
+    # (exact settings here depend on your ESP...)
+    "MAILGUN_API_KEY": MAILGUNAPIKEY,
+    "MAILGUN_SENDER_DOMAIN": EMAILHOST,  # your Mailgun domain, if needed
+}
+
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+DEFAULT_FROM_EMAIL = 'admin@oh-joy.org'
+SERVER_EMAIL = 'admin@oh-joy.org'
+EMAIL_HOST = EMAILHOST
+EMAIL_PORT = SMTPPORT
+EMAIL_HOST_USER = EMAILUSER
+EMAIL_HOST_PASSWORD = EMAILPASSWORD
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
